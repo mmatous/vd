c2c8ed369c7e06ebb568aa2dc2768def0891257a
Martin Matous <matous-dev@criptext.com>
2019-09-26 22:36:55 +0200

    add chocolatey files

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$extensionName = "vd@vd.io.xpi"
+
+$extensionsFolder = "$env:LOCALAPPDATA\Mozilla\Extensions\{ec8030f7-c20a-464f-9b0e-13a3a9e97384}"
+
+$isrunning = Get-Process -Name "firefox" -ErrorAction SilentlyContinue
+if ($isrunning) {
+    echo "Firefox needs to be restarted for changes to take place"
+}
+
+Get-ChocolateyUnzip -File  "$toolsDir\vd.7z" -Destination "$extensionsFolder"
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,4 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$vdFolder = "$env:LOCALAPPDATA\Mozilla\Extensions\{ec8030f7-c20a-464f-9b0e-13a3a9e97384}\vd@vd.io.xpi"
+Remove-Item "$vdFolder" -Force -Recurse
